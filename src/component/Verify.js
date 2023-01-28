f5f512326cb7e6f76ad0d51e42794fccf46ded00
ranavijaysingh <vijaysinghgr81@gmail.com>
2023-01-28 23:17:15 +0530

    Complete without resizing of the page

--- src/component/Verify.js
+++ src/component/Verify.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState} from "react";
 import { Link } from 'react-router-dom';
 import '../style/Verify.css';
-
+import Info from "./Info";
 
 const Verify = ({userData}) => {
-    return (
-<div className="section-head">
+  const [active, setActive] = useState("verify");
+  const [mobile, setMobile] = useState("")
+
+  const handleSubmit = () =>{
+    console.log(mobile)
+    if(userData.otp===mobile)
+          setActive("info");
+        }
+    
+
+  return (
+      <div>
+        {active==="verify" && <div className="section-head">
         {/* Constant Photo Section */}
         <div className="left-photo">
           {/* <img src={leftphoto} alt='left side background'/> */}
@@ -17,7 +28,7 @@ const Verify = ({userData}) => {
             <h5><b>{userData.name}</b></h5>
             <p>Plese verify your email id to gain access</p>
             <p>to the community</p>
-            <p className="paraInput"><span className="sp1">+{userData.number}</span><span className="sp2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+            <p className="paraInput"><span className="sp1">+{userData.number}</span><span className="sp2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2" viewBox="0 0 16 16">
   <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
 </svg>Verified</span></p>
             <div className="leftContent">
@@ -25,13 +36,18 @@ const Verify = ({userData}) => {
               <p>Please enter magic code set on </p>
               <p>+{userData.number}</p>
             </div>
-            <form className="otpForm">
-                <input type="text"></input>
-                <button>Confirm</button>
+            <form className="otpForm"  onSubmit={e => e.preventDefault()}>
+                <input type="text"
+                                  value={mobile}
+                                  onChange = { (e) => setMobile(e.target.value)}></input>
+                <button onClick={() => handleSubmit()}>Confirm</button>
             </form>            
           </center>
         </div>
+      </div>}
+      {active=="info" && <Info userData={userData}/>}
       </div>
+
     )
 };
 export default Verify;
